d3af78e1e8ffbfd005d7351f12326ce7af2a6e28
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-01 18:21:53 +0530

    database is connected

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/mongoDB.js";
 dotenv.config();
 
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
