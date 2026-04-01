28403f890af83e0f6c688486986628063e3df148
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-01 19:28:06 +0530

    user routes is added

--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/mongoDB.js";
+import userRoute from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());,
+app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/user", userRoute)
 
 app.use("/", (req, res) => {
   res.send("API WORKING!")
