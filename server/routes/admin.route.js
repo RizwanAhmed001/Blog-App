985d11329d6bbe813301ee9de251a9d7651ee53b
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-02 17:51:40 +0530

    admin logout api is done

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { adminLogin, adminRegister } from "../controllers/admin.controller.js";
+import { adminLogin, adminLogout, adminRegister } from "../controllers/admin.controller.js";
 
 const adminRoute = express.Router();
 
 adminRoute.post("/register", adminRegister);
 adminRoute.post("/login", adminLogin);
+adminRoute.post("/logout", adminLogout);
 
 export default adminRoute;
