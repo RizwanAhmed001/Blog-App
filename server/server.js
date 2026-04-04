725503f3edbc8d50c91f9f237a6bc1faa0658f0e
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-04 19:22:34 +0530

    multer and cloudinaryconfig working fine

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ const app = express();
 cloudinaryConfig();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
