5c92c63475394cd229452fd186198f47a0980631
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-07 19:17:27 +0530

    all api's working perfectly fine

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { adminLogin, adminLogout, adminRegister } from "../controllers/admin.controller.js";
 import upload from "../middlewares/multer.js";
-import { createBlog, deleteComment, toggleComment, togglePublished } from "../controllers/blog.controller.js";
+import { createBlog, deleteBlog, deleteComment, toggleComment, togglePublished } from "../controllers/blog.controller.js";
 import { adminAuth } from "../middlewares/admin.auth.js";
 
 const adminRoute = express.Router();
@@ -12,7 +12,8 @@ adminRoute.post("/logout", adminLogout);
 
 adminRoute.post("/newblog", adminAuth, upload.single("file"), createBlog);
 adminRoute.put("/togglestatus/:blogid", adminAuth, togglePublished);
-adminRoute.put("/toggleComment/:blogid:/commentid", adminAuth, toggleComment)
-adminRoute.delete("/comment/:blogid/:commentid", adminAuth, deleteComment)
+adminRoute.delete("/deleteblog/:blogid", adminAuth, deleteBlog);
+adminRoute.put("/togglecomment/:blogid/:commentid", adminAuth, toggleComment)
+adminRoute.delete("/deletecomment/:blogid/:commentid", adminAuth, deleteComment)
 
 export default adminRoute;
