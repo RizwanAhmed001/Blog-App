700dbff2edfb880e54be2b3db05351ddc239cc0b
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-08 19:23:28 +0530

    register functionality is done without api now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import Blog from "./pages/Blog";
+import Register from "./pages/Register";
+import Error from "./pages/Error";
 
+function App() {
   return (
-    <>
-      <div className="bg-green-400">Blog App</div>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route element={<Blogs />} path="/" />
+        <Route element={<Blog />} path="/:blogid" />
+        <Route element={<Register />} path="/register" />
+        <Route element={<Error />} path="*" />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { LuUser } from "react-icons/lu";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoLockClosedOutline } from "react-icons/io5";
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
+
+const Register = () => {
+  const [login, setLogin] = useState(true);
+  const [passwordVis, setPasswordVis] = useState(true);
+  return <div>
+
+    {!login && 
+    // Register
+      <form>
+
+        <h2><span>Re</span> <span>gistration</span></h2>
+
+        <div>
+          <LuUser />
+          <input type="text" placeholder="Enter your name" name="name" required />
+        </div>
+
+        <div>
+          <MdOutlineEmail />
+          <input type="email" placeholder="Enter your email" name="email" />
+        </div>
+
+        <div>
+          <IoLockClosedOutline />
+          <input type="password" placeholder="Create password" name="password" />
+        </div>
+
+        <div>
+          <IoLockClosedOutline />
+          <input type={passwordVis ? "password" : "text"} placeholder="Confirm password" name="confirm password" />
+
+          {
+            passwordVis ? <FaRegEye onClick={() => setPasswordVis(false)}/> : <FaRegEyeSlash onClick={() => setPasswordVis(true)} />
+          }
+
+        </div>
+
+        <button>Register Now</button>
+        
+        <div>Aready have an account <span onClick={() => setLogin(true)}>Login now</span></div>
+
+
+      </form>
+    }
+
+    {login && 
+    // Login 
+      <form>
+
+        <h2><span>Re</span> <span>gistration</span></h2>
+
+        <div>
+          <LuUser />
+          <input type="text" placeholder="Enter your name" name="name" required />
+        </div>
+
+        <div>
+          <MdOutlineEmail />
+          <input type="email" placeholder="Enter your email" name="email" />
+        </div>
+
+        <div>
+          <IoLockClosedOutline />
+          <input type="password" placeholder="Create password" name="password" />
+        </div>
+
+        <div>
+          <IoLockClosedOutline />
+          <input type={passwordVis ? "password" : "text"} placeholder="Confirm password" name="confirm password" />
+
+          {
+            passwordVis ? <FaRegEye onClick={() => setPasswordVis(false)}/> : <FaRegEyeSlash onClick={() => setPasswordVis(true)} />
+          }
+
+        </div>
+
+        <button>Register Now</button>
+        
+        <div>Aready have an account <span onClick={() => setLogin(true)}>Login now</span></div>
+      </form>
+    }
+    
+    </div>;
+};
+
+export default Register;
