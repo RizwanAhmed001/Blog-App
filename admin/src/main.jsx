cb47e80865702280ff7a8e522b9f4e0f91b01643
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-17 19:41:02 +0530

    react router is added for navigation

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AdminBlogProvider from "./context/AdminBlogProvider.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AdminBlogProvider>
-      <App />
-    </AdminBlogProvider>
+    <BrowserRouter>
+      <AdminBlogProvider>
+        <App />
+      </AdminBlogProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
