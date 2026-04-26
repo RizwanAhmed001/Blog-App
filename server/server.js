bce4b961ebd1e9d9db6d55761f06ec5083cfd420
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-26 18:41:11 +0530

    render client and admin url is added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 cloudinaryConfig();
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://blog-app-client-bkx8.onrender.com", "https://blog-app-admin.onrender.com"];
 
 app.use(
   cors({
